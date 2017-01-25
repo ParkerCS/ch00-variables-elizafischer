03ad679f25ce8d97c6bf1b9e955e602f3ef59cec
elizafischer <efischer@fwparker.org>
2017-01-25 08:46:45 -0600

    Changes

--- ch00_problem_set_1.py
+++ ch00_problem_set_1.py
@@ -1,3 +1,4 @@
+import math
 #SECTION 1 - MATH OPERATORS AND VARIABLES (20PTS TOTAL)
 
 #PROBLEM 1 (From Math Class to Code - 2pts)
@@ -6,24 +7,47 @@
 # 3(60x^2 + 3x/9) + 2x - 4/3(x) - sqrt(x)
 # where x = 12.83
 x = 12.83
-your_answer = "Enter the equation"
+your_answer = 3 * ((60 * (x**2) + (3 * (x)/9))  + (2*(x)) - (4/(3*(x))) - ( x ** 0.5 ))
 print(your_answer)
+print("")
+
 
 #PROBLEM 2 (Set your alarm - 3pts)
-
 #You look at the clock and see that it is currently 1:00PM.
 # You set an alarm to go off 728 hours later.
 # At what time will the alarm go off? Write a program that prints the answer.
 # Hint: for the best solution, you will need the modulo operator.
+clock = 1
+clock_time = str("1:00 PM" )
+print(clock_time)
+print("An alarm will go off 728 hours later than this time.")
+time = 728//12
+time = time//12
+clock += time
+print ("Alarm goes off at " + str(clock) +":00")
 
 
+
+print("")
 
 #PROBLEM 3 (Wholesale Books - 3pts)
 #The cover price of a book is $27.95, but bookstores get a 50 percent discount.
 #Shipping costs $4 for the first copy and 75 cents for each additional copy.
 # Calculate the total wholesale costs for 68 copies to the nearest penny.
+book = 27.95
+discount = 0.5
+subtract = book * discount
+print("Subtract = " + str(subtract))
+book -= subtract
+shipping = (68 - 1) * 0.75
+print("Shipping = " + str(shipping))
+total= book + shipping + 4
+float(total)
+total = (round(total, 2))
+print("Total wholesale cost: $" + str(total))
 
 
+print("")
 #PROBLEM 4 (Dining Room Chairs - 3pts)
 # You purchase eight chairs for your dining room.
 # You pay for the chairs plus sales tax at 9.5%
@@ -33,14 +57,17 @@ chair_price = 189.99
 tax_percent = 0.095
 units = 8
 
+chair_price_plustax = (chair_price * tax_percent) + chair_price
+print(chair_price_plustax)
+
 #PROBLEM 5 (Area of Circle - 3pts)
 # Write code that can compute the area of circle.
 # Create variables for radius and pi (3.14159)
 # The formula, in case you do not know, is radius times radius times pi.
 # Print the outcome of your program as follows:
-# “The surface area of a circle with radius ... is ...”
+# The surface area of a circle with radius ... is ...
 
-#PROBLEM 6 (Coin counter - 4pts)
+#PROBLEM 6 (Coin counter - 4pts) (modulo and floor operators, save results and add together)
 # Write code that classifies a given amount of money (which you store in a variable named count),
 # as greater monetary units. Your code lists the monetary equivalent in dollars, quarters,
 # dimes, nickels, and pennies.
@@ -49,6 +76,7 @@ units = 8
 # then the maximum number of dimes that fit in the remainder after you subtract the dollars and quarters,
 # and so on for nickels and pennies.
 # The result is that you express the amount as the minimum number of coins needed.
+
 
 #PROBLEM 7 (Variable Swap - 2pts)
 # Can you think of a way to swap the values of two variables that does not
