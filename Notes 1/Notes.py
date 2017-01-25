64b603ce95ed4795e3014175579e1fa580d950a3
elizafischer <efischer@fwparker.org>
2017-01-25 09:01:46 -0600

    Notes

---
+++ Notes 1/Notes.py
@@ -0,0 +1,109 @@
+
+import math
+print(5//2)
+print(13%3)
+print(8**0.5)
+
+pi = math.pi
+print(pi)
+print(math.cos(0.5))
+
+
+print(round(5.67,1))
+
+#print statements, multiple items
+print(4,3,5,6, "Score:" + str(300))
+print("hello", end= " ")
+print("world")
+
+
+
+# input function
+#my_input = input("Write a number:")
+#print(int(my_input) + 3)
+
+print("hello".upper())
+
+
+# str, int and float
+y = 3.2
+y = int(y)
+y = float(y)
+y = str(y)
+print("$" + y)
+
+# escape codes
+print("escape\" me\\ \n next \t line")
+# Concatenation
+
+
+# Loops
+# While loops
+done = False
+while not done:
+    print("hello")
+    done = False
+print("end loop")
+
+
+x = 0
+while x < 100:
+    print(x)
+    x += 5
+
+
+
+# For loops and ranges
+for i in range(10):
+    print(i)
+
+
+for i in range(10,20, 5):
+    print(i)
+
+
+# Keep a running total
+total = 0
+for i in range(1,101):
+    total += i
+print(total)
+
+# Nested loops
+for i in range(10)
+    for j in range(10):
+        for k in range(10):
+            for l in range(10):
+                pass
+
+for q in range(10):
+    for r in range(10):
+        print("*", end = " ")
+    print()
+
+# continue, break and else
+for i in range(10):
+    if i == 0:
+        continue
+    print(100/i)
+
+for i in range(-5, 5):
+    print(100/i)
+
+
+for i in range(100):
+    if i == 50:
+        break
+    print(i)
+
+for i in range(10):
+    print(i)
+    if i == 5:
+        break
+else:
+    print("done")
+
+
+
+
+
+
